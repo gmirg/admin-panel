5b08883498234836129655fc7814a5bc057d87ff
gmirg <gofthet@gmail.com>
2023-10-10 18:13:49 +0200

    building server & client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponentComponent } from './components/register-component/register-component.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { UserComponentComponent } from './components/user-component/user-component.component';
+import { AdminComponentComponent } from './components/admin-component/admin-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponentComponent,
+    LoginComponentComponent,
+    UserComponentComponent,
+    AdminComponentComponent
   ],
   imports: [
     BrowserModule,
